caa51551556f1cd7e66ab581cb7794733fbb2227
JanjeVuk <janjevuk@outlook.be>
2024-05-21 16:29:40 +0200

    Initial commit of Shattered plugin project.
    This initial commit includes primary file creation for the Shattered plugin project. Main.java, CommandManager.java, EventManager.java have been added under net.etum.shattered package. Configuration files for the project including .gitignore, pom.xml and IDE configuration files (in .idea folder) have also been included.

---
+++ src/main/java/net/etum/shattered/listeners/CommandManager.java
@@ -0,0 +1,8 @@
+package net.etum.shattered.listeners;
+
+import net.etum.shattered.Main;
+
+public class CommandManager {
+    public CommandManager(Main main) {
+    }
+}
---
+++ src/main/java/net/etum/shattered/listeners/EventManager.java
@@ -0,0 +1,8 @@
+package net.etum.shattered.listeners;
+
+import net.etum.shattered.Main;
+
+public class EventManager {
+    public EventManager(Main main) {
+    }
+}
