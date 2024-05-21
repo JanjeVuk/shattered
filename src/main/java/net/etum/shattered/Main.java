75a9713425327d6b313b672de9ccfebd1a402729
JanjeVuk <janjevuk@outlook.be>
2024-05-21 16:57:25 +0200

    Initial commit of Shattered plugin project.
    This initial commit includes primary file creation for the Shattered plugin project. Main.java, CommandManager.java, EventManager.java have been added under net.etum.shattered package. Configuration files for the project including .gitignore, pom.xml and IDE configuration files (in .idea folder) have also been included.

--- src/main/java/net/etum/shattered/Main.java
+++ src/main/java/net/etum/shattered/Main.java
@@ -5,19 +5,12 @@ import net.etum.shattered.listeners.CommandManager;
 import net.etum.shattered.listeners.EventManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Main class for the Shattered plugin.
- * This class is responsible for initializing and shutting down the plugin.
- */
+
 public final class Main extends JavaPlugin {
 
     // Singleton instance of the plugin
     private static Main instance;
 
-    /**
-     * Called when the plugin is enabled.
-     * Initializes command and event managers.
-     */
     @Override
     public void onEnable() {
         instance = this;
@@ -28,16 +21,13 @@ public final class Main extends JavaPlugin {
         // Initialize event manager
         new EventManager(this);
 
-        getLogger().info("ShatteredPlugin has been enabled!");
+        getLogger().info(getInstance().getName() + " has been enabled!");
     }
 
-    /**
-     * Called when the plugin is disabled.
-     * Use this method to handle any necessary cleanup.
-     */
+
     @Override
     public void onDisable() {
-        getLogger().info("ShatteredPlugin has been disabled!");
+        getLogger().info(getInstance().getName() + " has been disabled!");
     }
 
     public static Main getInstance() {
