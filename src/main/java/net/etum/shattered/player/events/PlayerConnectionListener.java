74cddc2d47d1a3f57f8dc6872715e4f0164853a9
no used <akianadtum@gmail.com>
2024-05-22 23:44:20 +0200

    Add PlayerConnectionListener to handle player events
    A new class, PlayerConnectionListener, has been added to the project. This class implements Listener and has event handlers for player join, quit, and login events.

---
+++ src/main/java/net/etum/shattered/player/events/PlayerConnectionListener.java
@@ -0,0 +1,31 @@
+package net.etum.shattered.player.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerConnectionListener implements Listener {
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+
+    }
+
+
+
+
+
+}
