8f3d2b4036e7ff5c17af2b0a64a87faa6045e013
no used <akianadtum@gmail.com>
2024-05-22 23:44:07 +0200

    Add player connection event registration in EventManager
    The EventManager class has been updated to include the registration process for player connection events. This is accomplished by creating a new private method called eventsPlayer where a new PlayerConnectionListener is registered with the game's PluginManager.

--- src/main/java/net/etum/shattered/listeners/EventManager.java
+++ src/main/java/net/etum/shattered/listeners/EventManager.java
@@ -1,8 +1,19 @@
 package net.etum.shattered.listeners;
 
 import net.etum.shattered.Main;
+import net.etum.shattered.player.events.PlayerConnectionListener;
+import org.bukkit.plugin.PluginManager;
 
 public class EventManager {
     public EventManager(Main main) {
+
+        PluginManager pm = main.getServer().getPluginManager();
+
+
+    }
+
+
+    private void eventsPlayer(Main main, PluginManager pm){
+        pm.registerEvents(new PlayerConnectionListener(), main);
     }
 }
