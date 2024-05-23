8fc4f6ae5d05ad58b20a9a14936d90ac7b04e376
JanjeVuk <janjevuk@outlook.be>
2024-05-23 16:53:53 +0200

    Update MainGui with enhanced player experience
    Enhanced the MainGui class by adding title and profile to the InventoryBuilder. Now, when the player opens the inventory, they will see a title "Shattered" and their profile with a player head icon, increasing their interactive experience.

--- src/main/java/net/etum/shattered/gui/MainGui.java
+++ src/main/java/net/etum/shattered/gui/MainGui.java
@@ -1,15 +1,24 @@
 package net.etum.shattered.gui;
 
 import net.etum.shattered.gui.builder.InventoryBuilder;
+import net.etum.shattered.gui.builder.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 public class MainGui implements Listener {
 
+    private static final String title = "Shattered";
+
 
     public static void open(Player player) {
-        InventoryBuilder inv = new InventoryBuilder("Main", 9*6);
+        InventoryBuilder inv = new InventoryBuilder(title, 9*6);
         inv.setTemplate();
+
+        ItemBuilder profil = new ItemBuilder(Material.PLAYER_HEAD);
+        profil.setName("Profil : " + player.getName());
+        profil.setSkullOwner(player.getName());
+
 
         player.openInventory(inv.build());
     }
