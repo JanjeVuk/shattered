a047bfc884a9796e9f9e176c5fe27a01745d860b
JanjeVuk <janjevuk@outlook.be>
2024-05-23 15:44:54 +0200

    Refactor PlayerConnectionListener class
    The PlayerConnectionListener class has been refactored to handle player login and logout events more efficiently. Functionality related to PlayerLoginEvent has been removed as it was not being used. In addition, a player's data is now loaded when they join and saved when they quit, improving data management and performance.

--- src/main/java/net/etum/shattered/player/events/PlayerConnectionListener.java
+++ src/main/java/net/etum/shattered/player/events/PlayerConnectionListener.java
@@ -1,31 +1,26 @@
 package net.etum.shattered.player.events;
 
+import net.etum.shattered.player.PlayerDataManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerConnectionListener implements Listener {
 
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
+        Player player = event.getPlayer();
+        PlayerDataManager playerDataManager = new PlayerDataManager(player);
+        playerDataManager.loadConfig();
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-
+        Player player = event.getPlayer();
+        PlayerDataManager.saveKnight(player);
+        PlayerDataManager.getAllKnights().remove(player.getUniqueId());
     }
-
-    @EventHandler
-    public void onPlayerLogin(PlayerLoginEvent event) {
-
-    }
-
-
-
-
 
 }
