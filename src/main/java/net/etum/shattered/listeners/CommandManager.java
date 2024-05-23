4aa2d2269d9d9010fef08df092140fd9acd64820
JanjeVuk <janjevuk@outlook.be>
2024-05-23 15:43:58 +0200

    Add CommandGui to CommandManager
    This update introduces the CommandGui to the CommandManager in the Main class. The `commandsGui` method has been added to handle CommandGui execution, ensuring that non-null commands are processed.

--- src/main/java/net/etum/shattered/listeners/CommandManager.java
+++ src/main/java/net/etum/shattered/listeners/CommandManager.java
@@ -1,8 +1,18 @@
 package net.etum.shattered.listeners;
 
 import net.etum.shattered.Main;
+import net.etum.shattered.player.commands.CommandGui;
+
+import java.util.Objects;
 
 public class CommandManager {
     public CommandManager(Main main) {
+
+        commandsGui(main);
+
+    }
+
+    private static void commandsGui(Main main){
+        Objects.requireNonNull(main.getCommand("menu")).setExecutor(new CommandGui());
     }
 }
