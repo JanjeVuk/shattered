d9729a4eb585064bc42ae16b8bc7a2d107fd55ee
JanjeVuk <janjevuk@outlook.be>
2024-05-23 16:53:34 +0200

    Add MainGui to event manager
    MainGui has been included in the event manager by incorporating it in the EventManager.java file. This will allow the handling of GUI events within the application alongside the already existing player events.

--- src/main/java/net/etum/shattered/listeners/EventManager.java
+++ src/main/java/net/etum/shattered/listeners/EventManager.java
@@ -1,6 +1,7 @@
 package net.etum.shattered.listeners;
 
 import net.etum.shattered.Main;
+import net.etum.shattered.gui.MainGui;
 import net.etum.shattered.player.events.PlayerConnectionListener;
 import org.bukkit.plugin.PluginManager;
 
@@ -10,11 +11,16 @@ public class EventManager {
         PluginManager pm = main.getServer().getPluginManager();
 
         eventsPlayer(main, pm);
+        eventsGui(main, pm);
 
     }
 
 
     private void eventsPlayer(Main main, PluginManager pm){
         pm.registerEvents(new PlayerConnectionListener(), main);
+    }
+
+    private void eventsGui(Main main, PluginManager pm){
+        pm.registerEvents(new MainGui(), main);
     }
 }
