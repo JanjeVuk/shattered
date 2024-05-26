add246ead453d87096dbd6ec2b28c9bd91e50f04
NadTum <janjevuk@outlook.be>
2024-05-26 21:59:49 +0200

    Add detailed comments to Pact class methods
    Expanded the comments in the Pact class to provide detailed explanation on the skills and mechanisms used in the pact with Azaroth. This includes the 'Break Deff', 'Blood Rage', and 'Sovereign Wrath' methods which represent different abilities for the character "bruiser".

--- src/main/java/net/etum/shattered/OverLord/azaroth/pact.java
+++ src/main/java/net/etum/shattered/OverLord/azaroth/pact.java
@@ -1,10 +1,49 @@
 package net.etum.shattered.OverLord.azaroth;
 
+
+/**
+ * La classe Pact représente le pacte avec Azaroth, l'Overlord de la colère.
+ * Ce pacte permet au chevalier de devenir un bruiser, efficace contre les tanks
+ * et lors de longs combats rapprochés.
+ */
+
 public class pact {
 
-    /*
 
+    /**
+     * Compétence 1 : Break Deff
+     *
+     * Mécanique :
+     * - Chaque attaque automatique du chevalier ajoute une pile.
+     * - À chaque 5 piles accumulées, l'armure de la cible est réduite de 3%.
+     * - La réduction d'armure dure 5 secondes.
+     * - Si la réduction d'armure expire, l'accumulation des piles recommence
+     *   et l'effet de réduction revient à 3% lors de la prochaine accumulation de 5 piles.
      */
+
+
+    /**
+     * Compétence 2 : Blood Rage
+     *
+     * Mécanique :
+     * - Lors de l'activation de cette capacité, le bruiser regagne des points de vie proportionnellement aux dégâts bruts infligés.
+     * - Cette capacité augmente les dégâts bruts infligés de 5%.
+     * - Si la cible est sous l'effet d'une réduction d'armure, l'augmentation des dégâts passe à 10%.
+     */
+
+    /**
+     * Compétence 3 :
+     */
+
+    /**
+     * Compétence 4 : Extention du territoire, Sovereign Wrath
+     *
+     * Mécanique :
+     * - Lorsque le domaine est activé, l'armure et la résistance magique est réduite à néant pour le bruiser et l'ennemie.
+     * - Les dégâts infligés par le bruiser sont transformé en dégât brut, les attaques peuvent crite.
+     * - La compétence Blood Rage est en permanance activé.
+     */
+
 
 
 }
